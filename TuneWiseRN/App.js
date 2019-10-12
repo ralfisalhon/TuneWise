edd43ce9d6bc01bda56c3217752c8ce7108c5088
Ralfi Salhon <rifat.salhon@tufts.edu>
2019-10-11 21:16:45 -0400

    Added SongPickScreen

--- TuneWiseRN/App.js
+++ TuneWiseRN/App.js
@@ -1,10 +1,12 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 var { MainScreen } = require("./screens/MainScreen.js");
 var { ListenScreen } = require("./screens/Listen.js");
+var { SongPickScreen } = require("./screens/SongPick.js");
 
 const MainNavigator = createStackNavigator({
   Home: { screen: MainScreen },
-  Listen: { screen: ListenScreen }
+  Listen: { screen: ListenScreen },
+  SongPick: { screen: SongPickScreen }
 });
 
 const App = createAppContainer(MainNavigator);
